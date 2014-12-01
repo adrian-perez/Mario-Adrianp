11c3be064017275fe78037c18b0b711f08d82ad4
Adrian Perez <perezA@appacademy.us>
2014-12-01 14:18:53 -0800

    kjbi

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -27,6 +27,9 @@ update: function(delta){
         this.body.vel.x = 0;
     } 
     
+    this.body.update(delta);
+    me.collisions.check(this, true, this.collideHandler.bind(this), true);
+    
     if(this.body.vel.x !== 0){
         if(!this.renderable.isCurrentAnimation("smallWalk")){
            this.renderable.setCurrentAnimation("smallWalk");
@@ -37,15 +40,27 @@ update: function(delta){
         this.renderable.setCurrentAnimation("idle");
     }
     
-    this.body.update(delta);
+    
     this._super(me.Entity, "update", [delta]);
     return true;
+},
+
+collideHandler: function(response){
+    
 }
 
 });
 
 me.LevelTrigger = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, settings]);
+        this.body.onCollision = this.onCollision.bind(this);
+        this.level = settings.level;
+    },
     
-    
+    onCollision: function(){
+        this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+        me.levelDirector.loadLevel(this.level);
+    }
     
 });
